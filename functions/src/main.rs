e3033098ba847c800a4be4cf2269b85f76c1a783
Sameer Kamran <sameerkamrandev@gmail.com>
2025-12-10 20:54:12 +0100

    functions done

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,21 @@ fn main() {
     hello_world();
     tell_height(180);
     human_id("Alice", 30, 165.0);
+
+    let x = {
+        let price = 5;
+        let quantity = 10;
+        price * quantity
+    };
+
+    println!("Total cost is: {}", x);
+    println!("Sum is: {}", add(10, 20));
+
+    let weight = 70.0;
+    let height = 1.75;
+
+    let result = calculate_bmi(weight, height);
+    println!("BMI is: {:.2}", result);
 }
 
 fn hello_world() {
@@ -17,5 +32,14 @@ fn human_id(name: &str, age: u32, height: f32) {
         "My Name is: {}, and Age is: {}, and Height is: {} cm",
         name, age, height
     );
-    
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// BMI = weight(kg) / height(m) ^2
+
+fn calculate_bmi(weight_kg: f64, height_m: f64) -> f64 {
+    weight_kg / (height_m * height_m)
 }
